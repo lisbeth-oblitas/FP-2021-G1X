756ee980068d819b6c6e55aa77471df8a7af166c
lisbethoblitas <>
2021-05-10 05:17:14 +0000

    borando repetidas

--- AreaTriangulo.py
+++
@@ -1,8 +0,0 @@
-#variable de entrada y otros
-print("ejercicio 01: Area Triangulo")
-#Datos de ingresados por dispositivos de entrada
-b=int(input("ingrese la base"))
-h=int(input("ingrese altura"))
-area=(b*h)/2
-#Datos de salida
-print("El area del triangulo es",area)
